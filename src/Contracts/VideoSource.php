a2bf19fc7932ac195b5b08cadd02445ff81bf0db
Juzaweb <dangtheanh16@gmail.com>
2024-04-20 17:33:58 +0700

    :+1: Youtube video source

--- src/Contracts/VideoSource.php
+++ src/Contracts/VideoSource.php
@@ -10,7 +10,11 @@
 
 namespace Juzaweb\Videos\Contracts;
 
+use Illuminate\Support\Collection;
+
 interface VideoSource
 {
+    public function get(string $video): array;
 
+    public function search(string $keyword): Collection;
 }
